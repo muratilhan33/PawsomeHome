c2ea0b9c6b41eea556f1dfdf7991528c547ad5c1
Murat İLHAN <muratilhan33@gmail.com>
2026-07-20 18:19:30 +0300

    support page

--- src/app/components/help/help.component.ts
+++ src/app/components/help/help.component.ts
@@ -9,5 +9,41 @@ import { RouterLink } from '@angular/router';
   styleUrl: './help.component.scss'
 })
 export class HelpComponent {
+  phoneNumber: string = '';
 
+  onPhoneInput(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    let input = inputElement.value;
+
+    let cleaned = input.replace(/(?!^\+)[^\d]/g, '');
+
+    let formatted = '';
+
+    if (cleaned.startsWith('+90')) {
+      const rest = cleaned.substring(3);
+      formatted = '+90';
+      if (rest.length > 0) formatted += ' ' + rest.substring(0, 3);
+      if (rest.length > 3) formatted += ' ' + rest.substring(3, 6);
+      if (rest.length > 6) formatted += ' ' + rest.substring(6, 8);
+      if (rest.length > 8) formatted += ' ' + rest.substring(8, 10);
+
+    } else if (cleaned.startsWith('05')) {
+      formatted = cleaned.substring(0, 4);
+      if (cleaned.length > 4) formatted += ' ' + cleaned.substring(4, 7);
+      if (cleaned.length > 7) formatted += ' ' + cleaned.substring(7, 9);
+      if (cleaned.length > 9) formatted += ' ' + cleaned.substring(9, 11);
+
+    } else if (cleaned.startsWith('5')) {
+      formatted = cleaned.substring(0, 3);
+      if (cleaned.length > 3) formatted += ' ' + cleaned.substring(3, 6);
+      if (cleaned.length > 6) formatted += ' ' + cleaned.substring(6, 8);
+      if (cleaned.length > 8) formatted += ' ' + cleaned.substring(8, 10);
+
+    } else {
+      formatted = cleaned.substring(0, 11);
+    }
+
+    inputElement.value = formatted;
+    this.phoneNumber = formatted;
+  }
 }
