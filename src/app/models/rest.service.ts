d777c86865028b00775b4e1a25cdd2fbc63e8e8d
Murat İLHAN <muratilhan33@gmail.com>
2026-09-20 22:56:23 +0300

    _redirects added, json-server changed

--- src/app/models/rest.service.ts
+++ src/app/models/rest.service.ts
@@ -1,28 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, shareReplay } from 'rxjs';
 import { Category } from './category.model';
 import { Paw } from './paw.model';
 import { City } from './city.model';
+import { DATA_URL } from '../config';
+
+interface AppData {
+  categories: Category[];
+  paws: Paw[];
+  cities: City[];
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
-  baseUrl: string = "http://localhost:3500/";
+  // baseUrl: string = "http://localhost:3500/";
+  private data$: Observable<AppData>;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.data$ = this.http.get<AppData>(DATA_URL).pipe(shareReplay(1));
+  }
 
   getCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>(this.baseUrl + 'categories');
+    // return this.http.get<Category[]>(this.baseUrl + 'categories');
+    return this.data$.pipe(map(data => data.categories));
   }
 
   getPaws(): Observable<Paw[]> {
-    return this.http.get<Paw[]>(this.baseUrl + 'paws');
+    // return this.http.get<Paw[]>(this.baseUrl + 'paws');
+    return this.data$.pipe(map(data => data.paws));
   }
 
   getCities(): Observable<City[]> {
-    return this.http.get<City[]>(this.baseUrl + 'cities');
+    // return this.http.get<City[]>(this.baseUrl + 'cities');
+    return this.data$.pipe(map(data => data.cities));
   }
 }
 
--- src/app/services/city.service.ts
+++ src/app/services/city.service.ts
@@ -1,16 +1,16 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { City } from '../models/city.model';
+import { RestService } from '../models/rest.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
   private baseUrl: string = "http://localhost:3500/";
-  constructor(private http: HttpClient) { }
+  constructor(private restService: RestService) { }
 
   getCities(): Observable<City[]> {
-    return this.http.get<City[]>(this.baseUrl + 'cities');
+    return this.restService.getCities();
   }
 }
